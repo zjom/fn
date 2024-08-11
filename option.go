be9e485394870331e60916bad093507f832c190d
Zihan Jin <admin@zihanjin.com>
2024-08-11 15:22:18 +1000

    option methods

--- option.go
+++ option.go
@@ -12,3 +12,31 @@ func Some[T any](v T) Option[T] {
 func None[T any]() Option[T] {
 	return Option[T]{}
 }
+
+func (o Option[T]) IsSome() bool {
+	return o.instantiated
+}
+
+func (o Option[T]) IsNone() bool {
+	return !o.instantiated
+}
+
+func (o Option[T]) Unwrap() T {
+	return o.value
+}
+
+func (o Option[T]) UnwrapOr(v T) T {
+	if o.instantiated {
+		return o.value
+	}
+
+	return v
+}
+
+func (o Option[T]) UnwrapOrElse(f func() T) T {
+	if o.instantiated {
+		return o.value
+	}
+
+	return f()
+}
